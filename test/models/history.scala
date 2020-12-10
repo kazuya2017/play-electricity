0c997eb37e6db0fd16ca7d6cc3cadca4cad8b4e2
kazuya2017 <kazuya.mk.moriki@furukawaelectric.com>
2020-12-10 13:35:35 +0900

    modified history.file

---
+++ test/models/history.scala
@@ -0,0 +1,13 @@
+import net.svil.bootcamp.electricity.Models._
+import org.scalatestplus.play._
+import java.time.LocalDateTime
+
+class CsvLoaderTest extends PlaySpec{
+    "history" must{
+    "return correct type of CsvLoader" in {
+        val src = getClass.getResource("HomeEnergyLogs.csv").getPath
+        val data = CsvLoader.load(src)
+        data mustBe a[Map[LocalDateTime, Long]]
+        }
+    }
+}
