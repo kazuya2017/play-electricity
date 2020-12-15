5363aeca7ea3ac2af00d84a57b31cf4bb5b6acc7
kazuya2017 <kazuya.mk.moriki@furukawaelectric.com>
2020-12-15 15:37:58 +0900

    added csvloader into history

--- app/models/history.scala
+++ app/models/history.scala
@@ -1,30 +1,29 @@
 package net.svil.bootcamp.electricity.Models
 
-import java.time.format.DateTimeFormatter
-import java.time.LocalDateTime
-import scala.io.Source
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.HashMap
+import java.time.LocalDateTime
+import scala.io.Source
+import java.time.format.DateTimeFormatter
 
-
-class History(val data: Map[LocalDateTime, Long])
+class History(val data: Map[LocalDateTime,Long])
 
 object CsvLoader{
-  def load(path: String): Map[LocalDateTime,Long] = {
-    val src = Source.fromFile(path)
-    var convertedRows  = Map.empty[LocalDateTime,Long]
-    val df = DateTimeFormatter.ofPattern("yyyy/MM/dd H[H]:mm:ss");
-    var count = 0
+    def load(path: String): Map[LocalDateTime, Long] = {
+      val src = Source.fromFile(path)
+      var convertedRows = Map.empty[LocalDateTime, Long]
+      val df = DateTimeFormatter.ofPattern("yyyy/MM/dd H[H]:mm:ss")
+      var count = 0
 
-    for ((line, index) <- src.getLines().zipWithIndex) {
-      val row = line.split(",").map(_.trim)
-      if(!row.isEmpty && index != 0 && count < 24*7 && row(0).contains(":00:"))
-      {
-        convertedRows += ( LocalDateTime.parse(row(0),df) -> row(2).toLong)
-        count = count + 1
+      for ((line, index) <- src.getLines().zipWithIndex){
+        val row = line.split(",").map(_.trim)
+        // data processing to csv
+        if(!row.isEmpty && index !=0 && count < 24*7 && row(0).contains(":00:")){
+          convertedRows = convertedRows + (LocalDateTime.parse(row(0), df) -> row(2).toLong)
+          count = count + 1
+        }
       }
-    }
-    src.close()
-    convertedRows
+      src.close()
+      convertedRows
     }
 }
--- test/models/history.scala
+++ test/models/history.scala
@@ -2,13 +2,13 @@ import net.svil.bootcamp.electricity.Models._
 import org.scalatestplus.play._
 import java.time.LocalDateTime
 
+// add csvloader into history
 class CsvLoaderTest extends PlaySpec{
     "history" must{
-        "return correct type of CsvLoader" in {
-            val data = CsvLoader.load("log.csv")
-            println(data.mkString(" "))
-            println(data.size)
-            data mustBe a[Map[LocalDateTime,Long]]
+    "return correct type of CsvLoader" in {
+        val src = getClass.getResource("HomeEnergyLogs.csv").getPath
+        val data = CsvLoader.load(src)
+        data mustBe a[Map[LocalDateTime, Long]]
         }
     }
 }
